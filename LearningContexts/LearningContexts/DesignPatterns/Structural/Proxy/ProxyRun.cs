98a1c95922002d73b13ec9e98737bc931ec3e7a5
Nethrazim <vlad.branzei@gmail.com>
2023-06-27 16:59:54 +0300

    + Proxy

---
+++ LearningContexts/LearningContexts/DesignPatterns/Structural/Proxy/ProxyRun.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningContexts.DesignPatterns.Structural.Proxy
+{
+    public abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    public class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    public class Proxy : Subject
+    {
+        private RealSubject realSubject;
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+            realSubject.Request();
+        }
+    }
+
+
+    internal class ProxyRun
+    {
+        public static void Run()
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+}
