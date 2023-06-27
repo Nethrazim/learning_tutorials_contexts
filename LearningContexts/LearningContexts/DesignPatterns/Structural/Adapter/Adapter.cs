c4491d415df923b467e3c82429719cf6f8bba725
Nethrazim <vlad.branzei@gmail.com>
2023-06-27 16:32:42 +0300

    + Adapter

---
+++ LearningContexts/LearningContexts/DesignPatterns/Structural/Adapter/Adapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningContexts.DesignPatterns.Structural.Adapter
+{
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    public class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+        public override void Request()
+        {
+            // Possibly do some other work
+            // and then call SpecificRequest
+            adaptee.SpecificRequest();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+
+    internal class AdapterRun
+    {
+        public static void Run()
+        {
+            Target target = new Adapter();
+            target.Request();
+        }
+    }
+}
